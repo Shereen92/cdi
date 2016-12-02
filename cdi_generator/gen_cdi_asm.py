42267e316caaff430de2ec42e7c3e3ab18e8801b
Sherin Hazboun <red_stars_92@hotmail.com>
2016-12-02 18:08:21 -0500

    Changes

--- cdi_generator/gen_cdi_asm.py
+++ cdi_generator/gen_cdi_asm.py
@@ -1,6 +1,7 @@
 import funct_cfg
 import operator
 import asm_parsing
+import sys
 
 def gen_cdi_asm(cfg, asm_file_descrs, options):
     """Writes cdi compliant assembly from cfg and assembly file descriptions"""
@@ -28,10 +29,12 @@ def gen_cdi_asm(cfg, asm_file_descrs, options):
             # can be reached from anywhere with sleds (Function pointers can
             # point to ANY function with the same signature, even static 
             # functions in a different translation unit)
-            asm_dest.write('.globl\t' + funct.uniq_label + '\n')
-            asm_dest.write(funct.uniq_label + ':\n')
+            #asm_dest.write('.globl\t' + funct.uniq_label + '\n')
+            debug_write(asm_dest, '.globl\t' + funct.uniq_label + '\n', functs, funct, apply_to_current_funct=True)
+            #asm_dest.write(funct.uniq_label + ':\n')
+            debug_write(asm_dest,funct.uniq_label + ':\n', functs, funct, apply_to_current_funct=True)
             #increment here by 2
-            increment_cdi_funct(functs, funct, 2, apply_to_current_funct=True)           
+            #increment_cdi_funct(functs, funct, 2, apply_to_current_funct=True)           
             
             funct.label_fixed_count = dict()
             for site in funct.sites:
@@ -49,7 +52,8 @@ def gen_cdi_asm(cfg, asm_file_descrs, options):
         # write the rest of the lines over
         src_line = asm_src.readline()
         while src_line:
-            asm_dest.write(src_line)
+            #asm_dest.write(src_line)
+            debug_write(asm_dest, src_line, all_functs, all_functs[-1])
             src_line = asm_src.readline()
         #code here
         print("We're here")
@@ -89,6 +93,11 @@ def increment_cdi_funct(functs, current_funct, amount, apply_to_current_funct=Fa
     ind = functs.index(current_funct)
     if(apply_to_current_funct is False):
         ind = ind + 1
+    
+    # Bounds check
+    if(ind >= len(functs)):
+        return
+        
     for item in functs[ind:]:
         item.asm_cdi_line_num += amount
 
@@ -188,6 +197,7 @@ def write_lines(num_lines, asm_src, asm_dest, dwarf_loc):
         asm_line = asm_src.readline()
         asm_parsing.update_dwarf_loc(asm_line, dwarf_loc)
         asm_dest.write(asm_line)
+        #debug_write(asm_dest, asm_line)
         i += 1
 
 def convert_to_cdi(site, funct, asm_line, asm_dest, cfg, 
@@ -203,8 +213,9 @@ def convert_to_cdi(site, funct, asm_line, asm_dest, cfg,
     elif site.group == site.INDIR_JMP_SITE:
         convert_indir_jmp_site(site, funct, asm_line, asm_dest)
     elif site.group == site.PLT_SITE:
-        asm_dest.write(asm_line)
-        increment_cdi_funct(functs, funct, 1)
+        #asm_dest.write(asm_line)
+        debug_write(asm_dest, asm_line, functs, funct)
+        #increment_cdi_funct(functs, funct, 1)
     else:
         eprint('warning: site has invalid type: line ' + site.asm_line_num, 
                 'in function named \'' + funct.asm_name + '\'')
@@ -228,9 +239,10 @@ def convert_call_site(site, funct, asm_line, asm_dest,
         globl_decl = ''
         if funct.asm_filename != site.targets[0].asm_filename:
             globl_decl = '.globl\t' + label + '\n'
-
-        asm_dest.write(asm_line + globl_decl + label + ':\n')
-        increment_cdi_funct(functs, funct, 2) #or 2?
+        
+        #asm_dest.write(asm_line + globl_decl + label + ':\n')
+        debug_write(asm_dest, asm_line + globl_decl + label + ':\n', functs, funct)
+        #increment_cdi_funct(functs, funct, c) #or 2?
         return
     
 
@@ -248,6 +260,7 @@ def convert_call_site(site, funct, asm_line, asm_dest,
         globl_decl = ''
         if funct.asm_filename != target.asm_filename:
             globl_decl = '.globl\t' + return_label + '\n'
+                
 
         call_sled += '1:\n'
         call_sled += '\tcmpq\t$' + target_name + ', ' + call_operand + '\n'
@@ -256,28 +269,55 @@ def convert_call_site(site, funct, asm_line, asm_dest,
         call_sled += globl_decl
         call_sled += return_label + ':\n'
         call_sled += '\tjmp\t2f\n'
-
+       
+        
     call_sled += '1:\n' # 8 complete lines
-    cdi_abort_line, cdi_abort_newlines = cdi_abort_str(sled_id_faucet(), funct.asm_filename, 
+    cdi_abort_line = cdi_abort_str(sled_id_faucet(), funct.asm_filename, 
             options['--no-abort-messages'], dwarf_loc)
     call_sled += cdi_abort_line
     call_sled += '2:\n'
     
-    increment_cdi_funct(functs, funct, 9 + cdi_abort_newlines)
-    asm_dest.write(call_sled)
-
+    #print("hi honey: " + call_sled)
+    #print("The result: " + str(c))
+    #increment_cdi_funct(functs, funct, c)
+    #asm_dest.write(call_sled)
+    debug_write(asm_dest, call_sled, functs, funct)
+    
+def super_newline_count(content):
+    quotation_mode = False
+    newline_count = 0
+    for char in content:
+        if quotation_mode == False:
+            if char == '\n':
+                newline_count += 1
+        if char == '\"':
+            quotation_mode = not quotation_mode
+        
+    return newline_count
+    
+def debug_write(file_handler, content, all_functs, funct, apply_to_current_funct=False):
+    num_new_lines = super_newline_count(content)
+    
+    sys.stdout.write("[ADDITIONAL LINES BEGIN (" + str(num_new_lines) +  ")]\n")
+    sys.stdout.write(content)
+    sys.stdout.write("[ADDITIONAL LINES END]\n")
+    sys.stdout.flush()
+    
+    increment_cdi_funct(all_functs, funct, num_new_lines, apply_to_current_funct)
+    file_handler.write(content)
         
 def convert_return_site(site, funct, asm_line, asm_dest, cfg,
         sled_id_faucet, dwarf_loc, options, functs):
     # don't fix 'main' in this version
     if funct.asm_name == 'main':
-        asm_dest.write(asm_line)
+        #asm_dest.write(asm_line)
+        debug_write(asm_dest, asm_line, functs, funct)
         return
 
     cdi_ret_prefix = '_CDI_' + funct.uniq_label + '_TO_'
 
     ret_sled= '\taddq $8, %rsp\n'
-
+    
     for target_label, multiplicity in site.targets.iteritems():
         i = 1
         while i <= multiplicity:
@@ -285,14 +325,15 @@ def convert_return_site(site, funct, asm_line, asm_dest, cfg,
             ret_sled += '\tcmpq\t$' + sled_label + ', -8(%rsp)\n'
             ret_sled += '\tje\t' + sled_label + '\n'
             i += 1
-    # 3*multiplicity + 1
-    cdi_abort_line, n = cdi_abort_str(sled_id_faucet(), funct.asm_filename,
+       
+    cdi_abort_line = cdi_abort_str(sled_id_faucet(), funct.asm_filename,
         options['--no-abort-messages'], dwarf_loc)
             
     ret_sled += cdi_abort_line
-    n +=  2 * (multiplicity-1) + 1
-    increment_cdi_funct(functs, funct, n)
-    asm_dest.write(ret_sled)
+   
+    #increment_cdi_funct(functs, funct, c)
+    #asm_dest.write(ret_sled)
+    debug_write(asm_dest, ret_sled, functs, funct)
 
 
 def convert_indir_jmp_site(site, funct, asm_line, asm_dest):
@@ -300,7 +341,7 @@ def convert_indir_jmp_site(site, funct, asm_line, asm_dest):
 
 def cdi_abort_str(sled_id, asm_filename, no_abort_msg, dwarf_loc):
     """Return string that aborts from cdi code with a useful debug message"""
-    n = 0 
+
     loc_str = asm_filename
     if dwarf_loc.valid():
         loc_str = str(dwarf_loc) + ':' + loc_str
@@ -308,16 +349,15 @@ def cdi_abort_str(sled_id, asm_filename, no_abort_msg, dwarf_loc):
     call_cdi_abort = ''
     if no_abort_msg:
         call_cdi_abort = '\tmovq\t$0, %rdx\n\tcall _CDI_abort\n'
-        n += 2
+
     else:
         call_cdi_abort += '\tmovq\t $.CDI_sled_id_' + str(sled_id) + ', %rsi\n'
         call_cdi_abort += '\tmovq\t$.CDI_sled_id_' + str(sled_id) +'_len, %rdx\n'
         call_cdi_abort += '\tcall\t_CDI_abort\n'
         call_cdi_abort += '.CDI_sled_id_' + str(sled_id) + ':\n'
-        call_cdi_abort += '\t.string\t"' + loc_str + ' id=' + str(sled_id) + '"\n'
+        call_cdi_abort += '\t.string\t\"' + loc_str + ' id=' + str(sled_id) + '\"\n'
         call_cdi_abort += '\t.set\t.CDI_sled_id_' + str(sled_id) + '_len, '
         call_cdi_abort += '.-.CDI_sled_id_' + str(sled_id) + '\n'
-        n += 6  
 
-    return call_cdi_abort, n
+    return call_cdi_abort
 
